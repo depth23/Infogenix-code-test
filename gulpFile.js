be59eba1fb23f9a04a1d043470bb743cda0fd79e
depth23 <deon.p.sharps@gmail.com>
2017-01-09 07:09:02 -0700

    removed folders

--- gulpFile.js
+++ gulpFile.js
@@ -10,33 +10,30 @@ var sass = require('gulp-sass');
 //Compile Sass
 gulp.task("styles", function() {
   "use strict";
-  gulp.src("./public/sass/**/*.scss")
+  gulp.src("sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./public/css/"));
+    .pipe(gulp.dest("css/"));
 });
 
 //Concatenate & Minify JS
-//uncomment below and delete this line on completion to for uglify, concatenate, and minify js files
 //gulp.task('scripts', function() {
 //  "use strict";
-//  gulp.src('./public/JS/*.js')
+//  gulp.src('./JS/*.js')
 //    .pipe(concat('all.js'))
-//    .pipe(gulp.dest('./public/dist'))
+//    .pipe(gulp.dest('./dist'))
 //    .pipe(rename('all.min.js'))
 //    .pipe(uglify())
-//    .pipe(gulp.dest('./public/dist/js'));
+//    .pipe(gulp.dest('./dist/js'));
 //});
 
 //Watch Files for Changes
 gulp.task('watch', function() {
   "use strict";
-//uncomment below and delete this line on completion to watch js files for uglify etc
-//  gulp.watch('./public/JS/*.js', ['scripts']);
-  gulp.watch("./public/sass/**/*.scss", ["styles"]);
+//  gulp.watch('./JS/*.js', ['scripts']);
+  gulp.watch("sass/**/*.scss", ["styles"]);
 });
 
 
 gulp.task('default', ['styles',
-//uncomment below and delete this line on completion to watch js files for uglify etc
 //                      'scripts',
                       'watch']);
