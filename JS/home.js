6da6fe4707c9fe4217d584811e9e132031ba86f9
depth23 <deon.p.sharps@gmail.com>
2017-01-10 19:37:45 -0700

    changed to responsive design

--- JS/home.js
+++ JS/home.js
@@ -4,16 +4,19 @@ var screenSize = window.screen.width;
 
 if(screenSize <= 415) {
   document.getElementById("burger-menu").addEventListener("click", function() {
-    document.getElementById('html').style.overflow = "hidden";
-    document.getElementById('body').style.overflow = "hidden";
+    document.getElementById('html').className += "no-scroll";
+    document.getElementById('body').className += "no-scroll";
     document.getElementById('header').style.height = "100vh";
-    document.getElementById('navBar').style.marginLeft = "0";
+    document.getElementById('navBar').style.display = "block";
+    document.getElementById('liOne').style.lineHeight = "20vh";
+    document.getElementById('liTwo').style.lineHeight = "20vh";
+    document.getElementById('liThree').style.lineHeight = "20vh";
   }, false);
   
   document.getElementById("navBar").addEventListener("click", function() {
-    document.getElementById('html').style.overflow = "auto";
-    document.getElementById('body').style.overflow = "auto";
-    document.getElementById('header').style.height = "83px";
-    document.getElementById('navBar').style.marginLeft = "102%";
+    document.getElementById('html').className = " ";
+    document.getElementById('body').className = " ";
+    document.getElementById('header').style.height = "160px";
+    document.getElementById('navBar').style.display = "none";
   }, false);
 }
